3b1c1da1302b9e70baebf0e83df14d5821e82576
SenthilKumar2k <senthil2492000@gmail.com>
2024-03-28 13:13:28 +0530

    tasks

--- tasks.py
+++ tasks.py
@@ -1,43 +1,58 @@
-# tasks.py (inside one of your Django apps)
-
-import os
-from celery import Celery  # Import Celery app instance
 from celery import shared_task
 from django.core.mail import send_mail
-from django.contrib.auth.models import User
 from django.conf import settings
-from datetime import datetime  # Add this import
+from datetime import datetime
+from settings.models import UserDetails
+import pytz
 
-# Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+# @shared_task
+# def send_bulk_emails(subject, message):
+#     print('hello')
+#     users=UserDetails.objects.all()
+#     emails=[user.email for user in users]
+#     batch_size=100
+#     for i in range(0, len(emails), batch_size):
+#         batch_emails=emails[i:i+batch_size]
+#         try:
+#             send_mail(subject, message, settings.EMAIL_HOST_USER, batch_emails)
+#         except Exception as e:
+#             print(f"error sending emails: {str(e)}")    
 
-app = Celery('your_project')
+# @shared_task
+# def schedule_bulk_email(subject,message, schedule_time):
+#     now=datetime.now()
+#     print(now)
+#     if schedule_time>now:
+#         send_bulk_emails.delay(subject, message)
+#     else:
+#         print("Schedule time should be in the future.")
 
-# Using a string here means the worker doesn't have to serialize the configuration object to child processes.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+"""1) The above version is more efficient for handling a large number of emails as it distributes 
+the workload across multiple tasks. However, it doesn't schedule tasks at the exact schedule_time.
 
-# Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+2) The below version ensures correctness by scheduling tasks to run at the exact schedule_time. 
+However, it may not be the most efficient approach for handling a large number of emails."""
 
 @shared_task
-def send_bulk_emails(subject, message):
-    users = User.objects.filter(email__isnull=False).exclude(email__exact='')  
-    # Filter users with valid emails
-    emails = [user.email for user in users if user.email]
-    
-    batch_size = 100  # Adjust the batch size as per your requirements
-    for i in range(0, len(emails), batch_size):
-        batch_emails = emails[i:i+batch_size]
-        try:
-            send_mail(subject, message, settings.EMAIL_HOST_USER, batch_emails)
-        except Exception as e:
-            # Log the error properly
-            print(f"Error sending emails: {str(e)}")
+def send_bulk_emails(subject, message, emails):
+    try:
+        send_mail(subject, message, settings.EMAIL_HOST_USER, emails)
+    except Exception as e:
+        print(f"error sending emails: {str(e)}")    
 
 @shared_task
-def schedule_bulk_email(subject, message, scheduled_time):
-    now = datetime.now()
-    if scheduled_time > now:
-        send_bulk_emails.apply_async((subject, message), eta=scheduled_time)
+def schedule_bulk_email(subject, message, schedule_time):
+    timezone=pytz.timezone('Asia/Kolkata')
+    now = datetime.now(timezone)
+    if schedule_time > now:
+        delta=(schedule_time-now).total_seconds()
+        users = UserDetails.objects.all()
+        emails = [user.email for user in users]
+        # batch_size = 100
+        # for i in range(0, len(emails), batch_size):
+        #     batch_emails = emails[i:i + batch_size]
+        send_bulk_emails.apply_async((subject, message, emails), countdown=delta)
     else:
-        print("Scheduled time should be in the future.")
+        print("Schedule time should be in the future.")
+
+
