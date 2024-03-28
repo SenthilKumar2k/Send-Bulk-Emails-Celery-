7f72d8235f178a3cfa08812c0df70289f84c606f
SenthilKumar2k <senthil2492000@gmail.com>
2024-03-28 13:12:36 +0530

    views

--- views.py
+++ views.py
@@ -1,26 +1,28 @@
-# views.py (inside one of your Django apps)
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .tasks import send_bulk_emails, schedule_bulk_email
+from orders.tasks import send_bulk_emails, schedule_bulk_email
+import pytz
 from datetime import datetime
 
 class BulkEmailView(APIView):
+    permission_classes=[AllowAny,]
     def post(self, request):
-        subject = request.data.get('subject')
-        message = request.data.get('message')
+        print('hello')
+        subject=request.data.get('subject')
+        message=request.data.get('message')
         send_bulk_emails.delay(subject, message)
-        return Response({"message": "Bulk emails sent successfully!"})
-    
+        return Response({"message":"Bulk email send successfully !"})
+
+
 class ScheduleBulkEmailView(APIView):
+    permission_classes=[AllowAny,]
     def post(self, request):
-        subject = request.data.get('subject')
-        message = request.data.get('message')
-        scheduled_time_str = request.data.get('scheduled_time')
-        
+        subject=request.data.get('subject')
+        message=request.data.get('message')
+        scheduled_time_str=request.data.get('scheduled_time')
+        timezone=pytz.timezone("Asia/Kolkata")
         try:
-            scheduled_time = datetime.strptime(scheduled_time_str, '%Y-%m-%d %H:%M:%S')
+            scheduled_time=datetime.strptime(scheduled_time_str, "%Y-%m-%d %H:%M:%S")
+            scheduled_time=timezone.localize(scheduled_time)
             schedule_bulk_email.delay(subject, message, scheduled_time)
-            return Response({"message": "Bulk email scheduled successfully!"})
+            return Response({"message":"Bulk Email sheduled successfully!"})
         except ValueError:
-            return Response({"message": "Invalid scheduled time format. Use YYYY-MM-DD HH:MM:SS."}, status=400)
+            return Response({"message":"Invalid schedule time format. Use YYYY-MM-DD HH:MM:SS"}, status=400)
